bc42ace6088218695561e3ef4074abc8087edd50
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-14 14:02:46 +0200

    Attempt to fix error by overriding retrieveUserInfo method.

--- src/Plugin/OpenIDConnectClient/OpenIDConnectEUFIDPClient.php
+++ src/Plugin/OpenIDConnectClient/OpenIDConnectEUFIDPClient.php
@@ -4,6 +4,9 @@ namespace Drupal\openid_connect_eufidp\Plugin\OpenIDConnectClient;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
+use Exception;
+use GuzzleHttp\ClientInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
  * OpenID Connect client for the EUF IDP.
@@ -14,6 +17,20 @@ use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
  * )
  */
 class OpenIDConnectEUFIDPClient extends OpenIDConnectClientBase {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * The logger factory used for logging.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
 
   /**
    * {@inheritdoc}
@@ -60,6 +77,36 @@ class OpenIDConnectEUFIDPClient extends OpenIDConnectClientBase {
       'token' => $this->configuration['token_endpoint'],
       'userinfo' => $this->configuration['userinfo_endpoint'],
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveUserInfo($access_token) {
+    $request_options = [
+      'headers' => [
+        'Authorization' => 'Bearer ' . $access_token,
+        'Accept' => 'application/json',
+      ],
+    ];
+    $endpoints = $this->getEndpoints();
+
+    $client = $this->httpClient;
+    try {
+      $response = $client->get($endpoints['userinfo'], $request_options);
+      $response_data = (string) $response->getBody();
+
+      return json_decode($response_data, TRUE);
+    }
+    catch (Exception $e) {
+      $variables = [
+        '@message' => 'Could not retrieve user profile information',
+        '@error_message' => $e->getMessage(),
+      ];
+      $this->loggerFactory->get('openid_connect_' . $this->pluginId)
+        ->error('@message. Details: @error_message', $variables);
+      return [];
+    }
   }
 
 }
